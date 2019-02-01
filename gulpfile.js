aac111f54a7361517d01eac801c5cad723bd34d1
Courtney Stevenson <stev0241@algonquinlive.com>
2019-02-01 13:38:23 -0500

    Added Gulp and Sass

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+
+
+gulp.task('sass', function() {
+  return gulp
+    .src('scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+})
